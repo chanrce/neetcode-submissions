7f31de70d6918a1d26d9dfb9a8ff8d9d976cf2e2
Rachel Chan <70292238+chanrce@users.noreply.github.com>
2026-07-16 13:35:22 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # l is buy, r is sell. r = 1 bc after buy
+        l = 0
+        r = 1
+        maxP = 0
+        #while r is in array
+        while r < len(prices):
+            #print("BEFORE | buy:", prices[l], "sell:", prices[r], "maxP:", maxP)
+            if prices[r]>prices[l]:
+                if prices[r]-prices[l]>maxP:
+                    maxP=prices[r]-prices[l]
+            else:
+                # if sell is less than buy, buy cheaper
+                # update buy with cheaper price
+                l=r
+                #print("l was updated")
+            #print("AFTER  | buy:", prices[l], "sell:", prices[r], "maxP:", maxP)
+            #print("------")
+            r=r+1
+        return maxP
+
+
+
