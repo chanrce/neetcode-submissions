a158dfa72c3d8d9550b4cf4adf54df06da96dc35
Rachel Chan <70292238+chanrce@users.noreply.github.com>
2026-07-27 10:13:21 -0400

    Add: can-place-flowers - submission-5

---
+++ Data Structures & Algorithms/can-place-flowers/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        # new array with 0s at ends
+        array=[0]+flowerbed+[0]
+        
+        # iterate through the array from original list indices
+        for index in range(1, len(array)-1):
+
+            if n == 0:
+                return True
+
+            if array[index] == 0 and array[index-1] == 0 and array[index+1] == 0:
+                array[index] = 1
+                n -= 1
+            if n==0:
+                return True
+        return False
