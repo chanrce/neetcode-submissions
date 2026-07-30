ae6cfaed55263aaccd2d4a0b58cd7c20a599dc35
Rachel Chan <70292238+chanrce@users.noreply.github.com>
2026-07-30 10:52:33 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # loop thru s and add all chars in s to a dict
+        seen = {}
+
+        if len(s)!=len(t):
+            return False
+
+        for char in s:
+            if char not in seen:
+                seen[char]=0
+            seen[char]+=1
+        # loop thru t and check if those match dictionary vals or not
+            # decrement from dict count each time it is seen
+        for char in t:
+            if char not in seen:
+                return False
+            if seen[char]==0:
+                return False
+            seen[char]-=1
+        return True
+
